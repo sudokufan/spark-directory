b8fd2ccde5b0cef7172edb569143b307357f272b
Alex Day <aday@hey.com>
2023-05-12 07:40:57 +0100

    converted function to const, started styling

--- src/walker.js
+++ src/walker.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './walker.css';
 
-function walker(name = 'Unknown', status = null, image = 'https://www.artofmtg.com/wp-content/uploads/2014/01/Totally-Lost-Art.jpg', source = 'https://mtg.fandom.com/wiki/Planeswalker/List_of_planeswalkers') {
+const walker = (name = 'Unknown', status = null, image = 'https://www.artofmtg.com/wp-content/uploads/2014/01/Totally-Lost-Art.jpg', source = 'https://mtg.fandom.com/wiki/Planeswalker/List_of_planeswalkers') => {
+
   return (
     <div className='walkerWrapper'>
       <a href={source} target="_blank" rel="noopener noreferrer">
       <img src={image} className='walkerImage' alt={name}/>
       </a>
+      // need some text to overlay on the image, outlining what happened and the source
         <p>
           {name}
         </p>
