be94a37d2a99df6475c0c7684b0e476272aeb26a
Alex Day <aday@hey.com>
2023-05-12 10:07:56 +0100

    installed prettier, made planeswalker array

--- src/App.js
+++ src/App.js
@@ -1,95 +1,101 @@
-import './App.css';
-import Walker from './walker';
+import "./App.css";
+import Walker from "./walker";
 
 const App = () => {
+  const walkers = [
+    {
+      name: "Kiora",
+      status: "desparked",
+      image:
+        "https://www.artofmtg.com/wp-content/uploads/2014/11/Kiora-the-Crashing-Wave-MtG-Art.jpg",
+      source: "https://scryfall.com/card/mat/35/kiora-sovereign-of-the-deep",
+      description: "Confirmed in March of the Machine: Aftermath",
+    },
+    { name: "Sally", role: "contributor" },
+  ];
+
   return (
     <div className="container">
       <Walker
-        name='Kiora'
-        status='desparked'
-        image='https://www.artofmtg.com/wp-content/uploads/2014/11/Kiora-the-Crashing-Wave-MtG-Art.jpg'
-        source='https://scryfall.com/card/mat/35/kiora-sovereign-of-the-deep'
-        description='Confirmed in March of the Machine: Aftermath'
+        name="Kiora"
+        status="desparked"
+        image="https://www.artofmtg.com/wp-content/uploads/2014/11/Kiora-the-Crashing-Wave-MtG-Art.jpg"
+        source="https://scryfall.com/card/mat/35/kiora-sovereign-of-the-deep"
+        description="Confirmed in March of the Machine: Aftermath"
       />
       <Walker
-        name='Nissa'
-        status='desparked'
-        image=''
-        source='https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world'
-        description='Confirmed on DailyMTG'
+        name="Nissa"
+        status="desparked"
+        image=""
+        source="https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world"
+        description="Confirmed on DailyMTG"
       />
       <Walker
-        name='Teferi'
-        status='desparked'
-        image=''
-        source='https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world'
-        description='Confirmed on DailyMTG'
+        name="Teferi"
+        status="desparked"
+        image=""
+        source="https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world"
+        description="Confirmed on DailyMTG"
       />
       <Walker
-        name='Karn'
-        status='desparked'
-        image=''
-        source='https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world'
-        description='Confirmed on DailyMTG'
+        name="Karn"
+        status="desparked"
+        image=""
+        source="https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world"
+        description="Confirmed on DailyMTG"
       />
       <Walker
-        name='Nahiri'
-        status='desparked'
-        image=''
-        source='https://magic.wizards.com/en/news/magic-story/beyond-repair'
-        description='Confirmed on DailyMTG'
+        name="Nahiri"
+        status="desparked"
+        image=""
+        source="https://magic.wizards.com/en/news/magic-story/beyond-repair"
+        description="Confirmed on DailyMTG"
+      />
+      <Walker name="Jace" status="unknown" image="" source="" description="" />
+      <Walker
+        name="Ajani"
+        status="planeswalker"
+        image=""
+        source=""
+        description=""
       />
       <Walker
-        name='Jace'
-        status='unknown'
-        image=''
-        source=''
-        description=''
+        name="The Wanderer"
+        status="desparked"
+        image="https://www.artofmtg.com/wp-content/uploads/2019/04/The-Wanderer-War-of-the-Spark-Art.jpg"
+        source="https://youtu.be/sImiwohnUYg?t=739"
+        description="Confirmed on Loading Ready Run"
       />
       <Walker
-        name='Ajani'
-        status='planeswalker'
-        image=''
-        source=''
-        description=''
+        name="Chandra"
+        status="planeswalker"
+        image=""
+        source="https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world"
+        description="Confirmed on DailyMTG"
       />
       <Walker
-        name='The Wanderer'
-        status='desparked'
-        image='https://www.artofmtg.com/wp-content/uploads/2019/04/The-Wanderer-War-of-the-Spark-Art.jpg'
-        source='https://youtu.be/sImiwohnUYg?t=739'
-        description='Confirmed on Loading Ready Run'
+        name="Will"
+        status="desparked"
+        image="https://www.artofmtg.com/wp-content/uploads/2018/05/Will-Kenrith-Battlebond-Art.jpg"
+        source="https://youtu.be/odzYBR-X_vw?t=1153"
+        description="Confirmed at Magiccon Minneapolis"
       />
       <Walker
-        name='Chandra'
-        status='planeswalker'
-        image=''
-        source='https://magic.wizards.com/en/news/magic-story/she-who-breaks-the-world'
-        description='Confirmed on DailyMTG'
+        name="Rowan"
+        status="desparked"
+        image="https://www.artofmtg.com/wp-content/uploads/2018/05/Rowan-Kenrith-Battlebond-Art.jpg"
+        source="https://youtu.be/odzYBR-X_vw?t=1153"
+        description="Confirmed at Magiccon Minneapolis"
       />
       <Walker
-        name='Will'
-        status='desparked'
-        image='https://www.artofmtg.com/wp-content/uploads/2018/05/Will-Kenrith-Battlebond-Art.jpg'
-        source='https://youtu.be/odzYBR-X_vw?t=1153'
-        description='Confirmed at Magiccon Minneapolis'
-      />
-      <Walker
-        name='Rowan'
-        status='desparked'
-        image='https://www.artofmtg.com/wp-content/uploads/2018/05/Rowan-Kenrith-Battlebond-Art.jpg'
-        source='https://youtu.be/odzYBR-X_vw?t=1153'
-        description='Confirmed at Magiccon Minneapolis'
-      />
-      <Walker
-        name='Ashiok'
-        status='planeswalker'
-        image='https://www.artofmtg.com/wp-content/uploads/2014/11/Kiora-the-Crashing-Wave-MtG-Art.jpg'
-        source='https://youtu.be/odzYBR-X_vw?t=1177'
-        description='Confirmed at Magiccon Minneapolis'
+        name="Ashiok"
+        status="planeswalker"
+        image="https://www.artofmtg.com/wp-content/uploads/2014/11/Kiora-the-Crashing-Wave-MtG-Art.jpg"
+        source="https://youtu.be/odzYBR-X_vw?t=1177"
+        description="Confirmed at Magiccon Minneapolis"
       />
     </div>
   );
-}
+};
 
 export default App;
