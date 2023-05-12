e116b9c4afec780b864807d6cc134bdde0e1edd9
Alex Day <aday@hey.com>
2023-05-12 08:07:43 +0100

    added status class tag, walker export

--- src/walker.js
+++ src/walker.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './walker.css';
 
-const walker = (name = 'Unknown', status = null, image = 'https://www.artofmtg.com/wp-content/uploads/2014/01/Totally-Lost-Art.jpg', source = 'https://mtg.fandom.com/wiki/Planeswalker/List_of_planeswalkers') => {
+const walker = (
+  name = 'Unknown',
+  status = null,
+  image = 'https://www.artofmtg.com/wp-content/uploads/2014/01/Totally-Lost-Art.jpg',
+  source = 'https://mtg.fandom.com/wiki/Planeswalker/List_of_planeswalkers'
+  ) => {
 
   return (
     <div className='walkerWrapper'>
       <a href={source} target="_blank" rel="noopener noreferrer">
-      <img src={image} className='walkerImage' alt={name}/>
+      <img src={image} className={`walkerImage ${status}`} alt={name}/>
       </a>
       // need some text to overlay on the image, outlining what happened and the source
         <p>
@@ -16,4 +21,4 @@ const walker = (name = 'Unknown', status = null, image = 'https://www.artofmtg.c
   );
 }
 
-export default App;
+export default Walker;
